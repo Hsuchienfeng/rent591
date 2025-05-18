0bee2d03f62c8d8339a95a583d6333f1147a6b5c
JOHNHSU <j1057kimo@gmail.com>
2025-05-18 13:06:44 +0800

    初始提交

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rent591 App
+
+用 Jetpack Compose 建立的 Android App，用來分析 591 中和區租屋資料，找出哪個路段租金最高。
---
+++ cd rent591.sh
@@ -0,0 +1,7 @@
+cd rent591
+git init
+git add .
+git commit -m "初始化 Rent591 專案"
+git remote add origin https://github.com/Hsuchienfeng/rent591.git
+git branch -M main
+git push -u origin main
